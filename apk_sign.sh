27a32660c024237cac46aa70aa1b59c6fc40f67d
JMGK <jmgk77@gmail.com>
2021-08-01 19:40:20 -0300

    working

---
+++ apk_sign.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android.keystore ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk android-app-key
+zipalign -v 4 ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk app-release.apk
+
--- www/js/index.js
+++ www/js/index.js
@@ -25,5 +25,49 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+
+    var zeroconf = cordova.plugins.zeroconf;
+    zeroconf.registerAddressFamily = 'ipv4'; // or 'ipv6' ('any' by default)
+    zeroconf.watchAddressFamily = 'ipv4'; // or 'ipv6' ('any' by default)
+
+
+    zeroconf.watch('_http._tcp.', 'local.', function (result) {
+        var action = result.action;
+        var service = result.service;
+        if (action == 'added') {
+            console.log('service added', service);
+        } else if (action == 'resolved') {
+            console.log('service resolved', service);
+            /* service : {
+            'domain' : 'local.',
+            'type' : '_http._tcp.',
+            'name': 'Becvert\'s iPad',
+            'port' : 80,
+            'hostname' : 'ipad-of-becvert.local',
+            'ipv4Addresses' : [ '192.168.1.125' ],
+            'ipv6Addresses' : [ '2001:0:5ef5:79fb:10cb:1dbf:3f57:feb0' ],
+            'txtRecord' : {
+                'foo' : 'bar'
+            } */
+            if (service.name == 'WIFIIR') {
+                var ip = 'http://' + service.ipv4Addresses[0];
+                var ref = cordova.InAppBrowser.open(ip, '_self', "location=no");
+            }
+        } else {
+            console.log('service removed', service);
+        }
+    }, function (result) {
+        console.log(result);
+    });
+
+    //   // publish a zeroconf service of your own
+    //   this.zeroconf.register('_http._tcp.', 'local.', 'Becvert\'s iPad', 80, {
+    //     'foo': 'bar'
+    //   }).then(result => {
+    //     console.log('Service registered', result.service);
+    //   });
+
+
+
     document.getElementById('deviceready').classList.add('ready');
 }
