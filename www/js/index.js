76f37498e716f12110ffa90206fa59e6ebb80866
JMGK <jmgk77@gmail.com>
2021-08-03 12:12:20 -0300

    icon; loader; code cleanup

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,15 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
+function loading_on() {
+    document.getElementById('load').style.display = 'block';
+    document.getElementById('app').style.display = 'none';
+}
+
+function loading_off() {
+    document.getElementById('load').style.display = 'none';
+    document.getElementById('app').style.display = 'block';
+}
+
 function onDeviceReady() {
     //
     var zeroconf = cordova.plugins.zeroconf;
@@ -11,12 +21,13 @@ function onDeviceReady() {
             if (result.service.name.toUpperCase().lastIndexOf('WIFIIR', 0) === 0) {
                 var ip = 'http://' + result.service.ipv4Addresses[0];
                 //#!#create <a href> in html
-                //#!#css stop wait animation
-                var ref = cordova.InAppBrowser.open(ip, '_self', 'location=no');
+                //css stop wait animation
+                loading_off();
+                var ref = cordova.InAppBrowser.open(ip, '_self', 'location=no,zoom=no,toolbar=no');
             }
         }
     });
 
-    //#!#css start wait animation
-    document.getElementById('deviceready').classList.add('ready');
+    //css start wait animation
+    loading_on();
 }
