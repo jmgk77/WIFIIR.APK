07584d5f099f80ea661b04ce9d75d9f5f16aa660
JMGK <jmgk77@gmail.com>
2021-08-06 12:34:22 -0300

    align multiple buttons

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,49 @@
 //main
 //console.log("MAIN");
 var zeroconf;
-var once;
+var iba;
 //set event handlers
 document.addEventListener('deviceready', onDeviceReady, false);
 document.addEventListener("pause", onPause, false);
 document.addEventListener("resume", onResume, false);
+
+//set mdns watch
+function set_watch() {
+    zeroconf.watch('_http._tcp.', 'local.', function (result) {
+        if (result.action == 'resolved') {
+            //console.log("FOUND!");
+            if (result.service.name.toUpperCase().lastIndexOf('WIFIIR', 0) === 0) {
+                var ip = 'http://' + result.service.ipv4Addresses[0];
+                //console.log(ip);
+
+                //create button
+                var div = document.createElement('div');
+                div.className = 'b';
+                document.getElementById('app').appendChild(div);
+
+                var button = document.createElement('button');
+                button.innerHTML = 'Open ' + result.service.name;
+                button.className = "c";
+                button.onclick = function () {
+                    cordova.InAppBrowser.open(ip, '_self', 'location=no,zoom=no,toolbar=no');
+                    return false;
+                };
+                document.getElementById('app').appendChild(button);
+
+                var div = document.createElement('div');
+                div.className = 'b';
+                document.getElementById('app').appendChild(div);
+
+                //open inbrowserapp on first found only
+                if (!iba) {
+                    //css stop wait animation
+                    loading_off();
+                    iba = once = cordova.InAppBrowser.open(ip, '_self', 'location=no,zoom=no,toolbar=no');
+                }
+            }
+        }
+    });
+}
 
 function onResume() {
     //console.log("ONRESUME");
@@ -15,7 +53,7 @@ function onResume() {
 
 function onPause() {
     //console.log("ONPAUSE");
-    // Handle the pause event
+    //remove watch
     zeroconf.unwatch('_http._tcp.', 'local.');
 }
 
@@ -32,34 +70,6 @@ function onDeviceReady() {
     set_watch();
 }
 
-//set mdns watch
-function set_watch() {
-    zeroconf.watch('_http._tcp.', 'local.', function (result) {
-        if (result.action == 'resolved') {
-            //console.log("FOUND!");
-            if (result.service.name.toUpperCase().lastIndexOf('WIFIIR', 0) === 0) {
-                var ip = 'http://' + result.service.ipv4Addresses[0];
-                //console.log(ip);
-
-                //create button
-                var button = document.createElement('button');
-                button.innerHTML = 'Open ' + result.service.name;
-                button.onclick = function () {
-                    cordova.InAppBrowser.open(ip, '_self', 'location=no,zoom=no,toolbar=no');
-                    return false;
-                };
-                document.getElementById('app').appendChild(button);
-
-                //open inbrowserapp on first found only
-                if (!once) {
-                    //css stop wait animation
-                    loading_off();
-                    once = cordova.InAppBrowser.open(ip, '_self', 'location=no,zoom=no,toolbar=no');
-                }
-            }
-        }
-    });
-}
 
 function loading_on() {
     document.getElementById('load').style.display = 'block';
